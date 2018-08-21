9991debf7cbf6c0518d4fbed462ae3d36196f09b
Cole Williams <wcolto@amazon.com>
2018-08-21 16:58:05 -0700

    Updated books defs and implementation.

--- inc/books.hpp
+++ inc/books.hpp
@@ -1,15 +1,20 @@
 #ifndef BOOKS_HPP
 #define BOOKS_HPP
 
-#define BOOK(x, y) {#x, #y}
+#define BOOK(x, y, z) {#x, #y, #z}
 #define TOTAL_BOOKS 66
 
 #include <string>
 
+/**
+ * @brief      A struct for containing the actual name of a book and a
+ * shortened name of the book.
+ */
 struct bookmark
 {
     std::string actual_name;
     std::string short_name;
+    std::string file_name;
 };
 
 extern bookmark book_list[];
